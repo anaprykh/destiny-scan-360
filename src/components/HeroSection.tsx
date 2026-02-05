7f631ae33387556c6bcbee37944a75e10aab7217
anaprykh <prihodko.nastia.2015@gmail.com>
2026-02-05 17:04:11 -0500

    Remove WhatsApp contact link from HeroSection
    Removed WhatsApp contact link from HeroSection.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,16 +20,7 @@ const HeroSection = ({ onBookClick }: HeroSectionProps) => {
       }}
     >
       {/* Overlay */}
-      <div className="mb-6 text-center">
-  <a
-    href={whatsappLink}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="text-lg font-semibold underline"
-  >
-    +1 (845) 607-8579
-  </a>
-</div>
+
 
       <div className="absolute inset-0 bg-background/70" />
       
